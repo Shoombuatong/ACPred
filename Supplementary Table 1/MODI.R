4cc43cf73335ff63d3410ca359962da5214026a6
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2019-03-04 14:00:41 +0500

    Update MODI.R

--- Supplementary Table 1/MODI.R
+++ Supplementary Table 1/MODI.R
@@ -1,4 +1,41 @@
-###### Feature is derived from Feature extraction.R
+#######set directory
+setwd('D:\\Peptide prediction\\Anticancer peptides\\Dataset')
+#######Load package
+library(Interpol)
+library(protr)
+library(seqinr)
+
+#######Extract AAC DPC TPC
+x <- read.fasta('ZOH.fasta', seqtype="AA", as.string = TRUE)
+label = read.csv("Label.csv", header = TRUE) 
+A <- x[(sapply(x, protcheck))]
+m = length(A)
+PCP  <- matrix(nrow = m, ncol = 531)
+pse = 3
+weight = 0.4
+PAAC <- matrix(nrow = m, ncol = 20 + pse)
+
+AAC <- t(sapply(A, extractAAC))
+DPC <- t(sapply(A, extractDC))
+TPC <- t(sapply(A, extractTC))
+
+for(i in 1:m){ 
+x = A[[i]][1]
+b = AAdescriptor(x, 531,2)
+PCP[i,] = Interpol(b, 531,"linear")
+}
+
+### Please note that PAAC and APAAC are PseAAC and Am-PseAAC, respectively.
+for(i in 1:m){ 
+PAAC[i, ] = extractPAAC(A[[i]][1],lambda = pse, w = weight, props = c("Hydrophobicity", "Hydrophilicity", "SideChainMass"))
+}
+
+col = 20+ 2*4
+APAAC  <- matrix(nrow = length(A), ncol = col)
+for (i in 1:length(A)){
+APAAC[i,] = extractAPAAC(A[[i]][1],lambda = 4, w = 0.01, customprops = NULL)
+}
+
 ############# MODI for internal
 Dat <- data.frame(AAC,PAAC,APAAC,Class = label)
 n = ncol(Dat)
